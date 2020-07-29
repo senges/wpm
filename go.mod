d3e3c8dcfff62889032af53ad756d7ee036d4742
charles.senges.etu <charles.senges.etu@univ-lille.fr>
2020-07-29 15:11:49 +0200

    fix(core): switch to go-git v5 to fix compatibility issues

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/senges/wpm
 go 1.14
 
 require (
+	github.com/go-git/go-git/v5 v5.1.0
 	github.com/pelletier/go-toml v1.8.0
 	github.com/urfave/cli/v2 v2.2.0
-	gopkg.in/src-d/go-git.v4 v4.13.1
 )
--- vcs.go
+++ vcs.go
@@ -1,78 +1,22 @@
 package main
 
 import (
-	"fmt"
-	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/config"
-	"gopkg.in/src-d/go-git.v4/plumbing"
+	git "github.com/go-git/go-git/v5"
+	"os"
 )
 
-func LocalClone() {
+func CloneTo(envName string) {
+	env := ConfigFile.Environment[envName]
 	url := ConfigFile.VCS
-	path := ConfigFile.Environment["local"].WpPath
 
-	_, err := git.PlainClone(path, false, &git.CloneOptions{URL: url})
-	if err != nil {
-		panic(err)
-	}
-}
+	INFO("Environment (%s) appears to be empty", envName)
+	INFO("Deploying last wordpress version to `%v' on host [%v@%v]", env.WpPath, env.Username, env.Host)
+	CMD("git -C %s clone %s", env.WpPath, url)
 
-func LocalFetch() {
-	path := ConfigFile.Environment["local"].WpPath
-
-	r, err := git.PlainOpen(path)
-	if err != nil {
-		panic(err)
-	}
-
-	w, err := r.Worktree()
-	if err != nil {
-		panic(err)
-	}
-
-	/* TODO */
-	/* Should properly handle 'already up-to-date' error later */
-	err = w.Pull(&git.PullOptions{RemoteName: "origin"})
-	if err != nil {
-		panic(err)
-	}
-}
-
-func SwitchBranch() {
-	path := ConfigFile.Environment["local"].WpPath
-
-	r, err := git.PlainOpen(path)
-	if err != nil {
-		panic(err)
-	}
-
-	w, err := r.Worktree()
-	if err != nil {
-		panic(err)
-	}
-
-	err = r.Fetch(&git.FetchOptions{
-		RefSpecs: []config.RefSpec{"refs/*:refs/*", "HEAD:refs/heads/HEAD"},
+	_, err := git.PlainClone(env.WpPath, false, &git.CloneOptions{
+		URL: url,
+		Progress: os.Stdout,
 	})
-	if err != nil {
-		fmt.Println(err)
-	}
 
-	/* TODO */
-	/* Fix issue 'reference not found' */
-	err = w.Checkout(&git.CheckoutOptions{
-		Branch: plumbing.NewBranchReferenceName(fmt.Sprintf("refs/heads/%s", CurrentEnv)),
-		Force:  true,
-	})
-	if err != nil {
-		panic(err)
-	}
-}
-
-func RemoteClone() {
-	/* TODO */
-}
-
-func RemoteFetch() {
-	/* TODO */
+	CheckIfError(err)
 }
